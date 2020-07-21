944d16bd3fd82f59984e29f7a7b03940e9d81f4d
Yash Goyal <f20180285@pilani.bits-pilani.ac.in>
2020-07-21 10:17:29 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListingComponent } from './listing/listing.component';
+import { RequirementsComponent } from './requirements/requirements.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+
+  {
+    path:'home',
+    component: HomeComponent,
+  },
+
+  {
+    path:'listings',
+    component:ListingComponent,
+   
+  },
+
+  {
+    path:'requirements',
+    component: RequirementsComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ListingComponent } from './listing/listing.component';
+import { RequirementsComponent } from './requirements/requirements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ListingComponent,
+    RequirementsComponent
   ],
   imports: [
     BrowserModule,
